d691e173c75342e361320890b5b2d7c707fdb282
mtchwin <93198494+mtchwin@users.noreply.github.com>
2023-03-27 15:08:37 -0700

    deck generation

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -7,17 +7,20 @@ signal requestComplete
 #######################
 var rng = RandomNumberGenerator.new()
 var json = JSON.new()
+var loadedSetDict = {}
 var loadedCardDict = {}
 var loadedCardImage = Image.new()
 var searchTarget = "base1-4"
 var url = "https://api.pokemontcg.io/v2/cards/"+searchTarget
+
 var gameScope = "table"
+var playerSlot = "1"
 #######################
 #-INITIALIZATION-------
 #######################
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("SCENE START")
+	print("SCENE START")	
 	loadRandomCardInViewer()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -25,6 +28,14 @@ func _process(delta):
 #######################
 #-API CALLS------------
 #######################
+func _on_set_request_request_completed(result, response_code, headers, body):
+	var error = json.parse(body.get_string_from_utf8())
+	if error != OK:
+		print("An error occurred trying to get the set.")
+		return
+	var response = json.get_data()
+	loadedSetDict = response["data"]
+	print("[GET SET] Data Loaded")
 func _on_card_request_request_completed(result, response_code, headers, body):
 	var error = json.parse(body.get_string_from_utf8())
 	if error != OK:
@@ -59,18 +70,37 @@ func _on_table_pressed():
 	$UI_table.visible = true
 	$UI_viewer.visible = false
 	loadRandomCardInViewer()
+func _on_gen_deck_pressed():
+	var cardList = Array.new()
+	for n in 60:
+		cardlist.Add("base1-"+str(rng.randi_range(1,102))
+	generateDeck(cardList)
 #######################
 #-CARD VIEWER----------
 #######################
 func loadRandomCardInViewer():
+	var texture = load("res://.godot/imported/cardBack.png-1d8732877f92ce0c993bbb589758e78d.ctex")
+	var material = StandardMaterial3D.new()
+	material.set_texture(0,texture)
+	material.no_depth_test = true
+	get_node("3D_OBJECTS/viewer/card").set_surface_override_material(0,material)
 	var idInSet = rng.randi_range(1,102)
 	searchTarget = "base1-"+str(idInSet)
 	url = "https://api.pokemontcg.io/v2/cards/"+searchTarget
 	$cardRequest.request(url)
 	await requestComplete
 	print("[AWAIT FINISHED] requestCompleted")
-	var texture = ImageTexture.create_from_image(loadedCardImage)
-	var material = StandardMaterial3D.new()
+	texture = ImageTexture.create_from_image(loadedCardImage)
+	material = StandardMaterial3D.new()
 	material.set_texture(0,texture)
 	material.no_depth_test = true
 	get_node("3D_OBJECTS/viewer/card").set_surface_override_material(0,material)
+#######################
+#-GAME FUNCTIONS-------
+#######################
+func generateDeck(cardList):
+	var deckLocation = get_node("3D_OBJECTS/table/p"+playerSlot+"/deck").global_position
+	var card
+	for n in 60:
+		card = load()
+	
